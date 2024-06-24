000b84403489a08e917c30e8fc0cdb6c41e0f0e9
vicotirah <vicotira.rocha@gmail.com>
2024-06-24 18:23:31 -0300

    teste

--- app.js
+++ app.js
@@ -3,5 +3,7 @@ let numeroSecreto = 5;
 let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-    console.log('Parabéns!Você descobriu o número secreto');
+    alert('Parabéns!Você descobriu o número secreto');
+} else {
+    alert('Você errou :(')
 }
